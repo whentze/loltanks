3f1014ec0b1219b0d71533dd1759b3da86cc582b
muvlon <muvlon@hentzes.de>
2016-08-01 05:05:32 +0200

    remove redudancy in configs

--- configs.py
+++ configs.py
@@ -24,26 +24,12 @@ basic_conf = {
   'shot_tail'        : 7,
 }
 
-nice_conf = {
-  'gamename'        : 'loltanks',
-  'gravity'         : 10,
+nice_conf = dict(basic_conf, **{
   'sky_min'         : 0.4,
   'ground_min'      : 0.2,
   'ground_steepness': 0.4,
   'world_style'     : 'Dirt',
   'world_border'    : 'Loop',
   'players_number'  : 2,
-  'players_colors'  : [curses.COLOR_RED, curses.COLOR_BLUE, curses.COLOR_GREEN, curses.COLOR_YELLOW],
-  'tank_health'     : 100,
-  'tank_fuel'       : 50,
-  'tank_power'      : 1.5,
-  'wind_max'        : 5,
-  'wind_change'     : 2,
-  'wind_force'      : 0.0010,
   'particles_type'  : 'Stars',
-  'particles_max'   : 3.0,
-  'explosion_damage': 30,
-  'explosion_radius': 5,
-  'shot_age'        : 500,
-  'shot_tail'        : 7,
-}
+})
